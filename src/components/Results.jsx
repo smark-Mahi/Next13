dd10120bb8fd4c693b4fa54c2fe25f456f0c2092
smark-mahi <sumeriamahi1906@gmail.com>
2023-02-11 21:10:00 +0530

    movies

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,6 +1,7 @@
 import Card from "./Card";
 
 export default function Results({ results }) {
+  console.log(results)
   return (
     <div className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 max-w-6xl mx-auto py-4">
       {results.map((result) => (
