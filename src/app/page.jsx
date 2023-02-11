696308a39aa34a67da5eaaab7e383a5bdc886318
smark-mahi <sumeriamahi1906@gmail.com>
2023-02-11 18:30:39 +0530

    movies

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,4 @@
-// Fix the problem for "search params object is empty in production with next 13 app dir"
 
-// for more info "https://github.com/vercel/next.js/issues/43077"
 
 export const dynamic = "force-dynamic"; // this is the fix
 
